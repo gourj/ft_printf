f584871d259c1decc0684999ae87c977b3a96761
Aram Gurdzhinyan <agurdzhi@c2r15s3.42yerevan.am>
2025-05-15 22:03:06 +0400

    corrected test.c accordingly to The Norm

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: agurdzhi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:58:45 by agurdzhi          #+#    #+#             */
-/*   Updated: 2025/03/29 16:33:50 by agurdzhi         ###   ########.fr       */
+/*   Updated: 2025/05/15 22:01:34 by agurdzhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@
 int	main(void)
 {
 	const int	n = 2147483647;
+
 	ft_printf("printf(): ");
 	printf("");
 	ft_printf("\nft_printf()");
 	ft_printf("");
 	ft_printf("\n");
 	ft_printf("printf(): ");
-	printf("%c, %s, %p, %d, %i, \n%u, %x, %X, %%\n\n", 'a', "bcd", &n, n, n, -n, n, n);
+	printf("%c, %s, %p, %d, %i, \n%u, %x, %X, %%\n\n",
+		'a', "bcd", &n, n, n, -n, n, n);
 	ft_printf("ft_printf(): ");
-	ft_printf("%c, %s, %p, %d, %i, \n%u, %x, %X, %%\n", 'a', "bcd", &n, n, n, -n, n, n);
+	ft_printf("%c, %s, %p, %d, %i, \n%u, %x, %X, %%\n",
+		'a', "bcd", &n, n, n, -n, n, n);
 	return (0);
 }
